93ced3300e4c3d04b293206321d8e21aa0dc68d6
Matthew Foster <matthew.foster@hakai.org>
2022-10-03 10:53:48 -0700

    fix field filter when field value contains spaces

--- ckanext/harvest/harvesters/ckanharvester.py
+++ ckanext/harvest/harvesters/ckanharvester.py
@@ -6,7 +6,7 @@ from requests.exceptions import HTTPError, RequestException
 import datetime
 from urllib3.contrib import pyopenssl
 
-from six.moves.urllib.parse import urlencode
+from six.moves.urllib.parse import urlencode, quote_plus
 from ckan import model
 from ckan.logic import ValidationError, NotFound, get_action
 from ckan.lib.helpers import json
@@ -241,11 +241,11 @@ class CKANHarvester(HarvesterBase):
         field_filter_exclude = self.config.get('field_filter_exclude', [])
         if field_filter_include:
             fq_terms.append(' OR '.join(
-                '%s:%s' % (item['field'], item['value']) for item in field_filter_include
+                '%s:"%s"' % (item['field'], item['value']) for item in field_filter_include
             ))
         elif field_filter_exclude:
             fq_terms.extend(
-                '-%s:%s' % (item['field'], item['value']) for item in field_filter_exclude
+                '-%s:"%s"' % (item['field'], item['value']) for item in field_filter_exclude
             )
 
         # Ideally we can request from the remote CKAN only those datasets
